d0560c50b495826197a033767f11f72d49642e6f
ivysonin <ivysonthauan4@gmail.com>
2025-03-07 09:42:04 -0300

    Usei a função url_for() para deixar o código mais limpo e dinâmico

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+@app.route('/novapágina')
+def novapag():
+    return 'Segunda página web'
