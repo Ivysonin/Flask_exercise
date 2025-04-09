22246061c04865264bec35785faf5a3b058a7f8e
ivysonin <ivysonthauan4@gmail.com>
2025-04-09 17:56:19 -0300

    Criando a parte do método POST, e enviando ao banco de dados as informações

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from app import app
+from app import app, db
 from flask import render_template, url_for, request
+from app.models import Contato
 
 @app.route('/')
 def homepage():
@@ -9,11 +10,26 @@ def homepage():
     }
     return render_template('index.html', context=context)
 
-@app.route('/contato/')
+@app.route('/contato/', methods=['GET', 'POST'])
 def novapag():
     context = {}
     if request.method == 'GET':
         pesquisa = request.args.get('pesquisa')
         context.update({'pesquisa':pesquisa})
+
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        assunto = request.form['assunto']
+        mensagem = request.form['mensagem']
+
+        contato = Contato(
+            nome=nome,
+            email=email,
+            assunto=assunto,
+            mensagem=mensagem
+        )
+        db.session.add(contato)
+        db.session.commit()
 
     return render_template('contato.html', context=context)
