ffd00821a3b1484c3d991ce37772bdd9fa5fa0d2
ivysonin <ivysonthauan4@gmail.com>
2025-04-18 17:29:08 -0300

    Deixei o código muito mais dinâmico e otimizado(modelo "padrão"

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from app import app, db
-from flask import render_template, url_for, request
-from app.models import Contato
+from app import app
+from flask import render_template, url_for, redirect
+from app.forms import ContatoForm
 
 @app.route('/')
 def homepage():
@@ -10,26 +10,13 @@ def homepage():
     }
     return render_template('index.html', context=context)
 
+
 @app.route('/contato/', methods=['GET', 'POST'])
-def novapag():
+def contato():
+    form = ContatoForm()
     context = {}
-    if request.method == 'GET':
-        pesquisa = request.args.get('pesquisa')
-        context.update({'pesquisa':pesquisa})
-
-    if request.method == 'POST':
-        nome = request.form['nome']
-        email = request.form['email']
-        assunto = request.form['assunto']
-        mensagem = request.form['mensagem']
-
-        contato = Contato(
-            nome=nome,
-            email=email,
-            assunto=assunto,
-            mensagem=mensagem
-        )
-        db.session.add(contato)
-        db.session.commit()
-
-    return render_template('contato.html', context=context)
+    if form.validate_on_submit():
+        form.save()
+        return redirect(url_for('homepage'))
+    
+    return render_template('contato.html', context=context, form=form)
