0443c0439d06cbb8536b56e9add243e329894530
ivysonin <ivysonthauan4@gmail.com>
2025-06-02 16:53:26 -0300

    Criando a tabela(models) do usuário

--- app/models.py
+++ app/models.py
@@ -1,5 +1,21 @@
-from app import db
+from app import db, login_manager
 from datetime import datetime
+from flask_login import UserMixin
+
+
+# Retorna o usuário a partir do ID
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+# 'UserMixin' usado para informar que essa tabela é de usuário para controle de login
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String, nullable=False)
+    sobrenome = db.Column(db.String, nullable=False)
+    email =  db.Column(db.String, nullable=False)
+    senha = db.Column(db.String, nullable=False)
+
 
 class Contato(db.Model):
     id = db.Column(db.Integer, primary_key=True)
