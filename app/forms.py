7a8c325e09886031e56c1774d9e5505acb9ebf0d
ivysonin <ivysonthauan4@gmail.com>
2025-06-11 21:07:07 -0300

    Criado form para cadastrar o usuário

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,36 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import data_required, Email
-from app import db
-from app.models import Contato
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import data_required, Email, EqualTo, ValidationError
+from app import db, bcrypt
+from app.models import Contato, User
+
+
+class UserForm(FlaskForm):
+    nome = StringField('Nome', validators=[data_required()])
+    sobrenome = StringField('Sobrenome', validators=[data_required()])
+    email = StringField('E-Mail', validators=[data_required(), Email()])
+    senha = PasswordField('Senha', validators=[data_required()])
+    confirmacao_senha = PasswordField('Senha', validators=[data_required(), EqualTo('senha')])
+    btnSubmit = SubmitField('Cadastrar')
+
+    def validate_email(self, email):
+        # Estou acessando o db e procurando se tem algum 'email' igual, se tiver retorna um erro.
+        if User.query.filter(email=email.data).first():
+            return ValidationError('Usuário Já Cadastrado com esse E-mail!!!')
+
+    def save(self):
+        # Criptografando a senha
+        senha = bcrypt.generate_password_hash(self.senha.data.encode('utf-8'))
+        user = User(
+            nome = self.nome.data,
+            sobrenome = self.sobrenome.data,
+            email = self.email.data,
+            senha = senha
+        )
+        db.session.add(user)
+        db.session.commit()
+        return user
+
 
 class ContatoForm(FlaskForm):
     # Sempre passa primeiro o nome.
